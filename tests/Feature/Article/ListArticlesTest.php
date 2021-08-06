fc76d121386fb93bca48bc61c3d0359f1a5a8433
Diego Gonzalez <gonzalezdiego1990@gmail.com>
2021-08-06 10:23:52 -0300

    "Inicio de proyecto"

---
+++ tests/Feature/Article/ListArticlesTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Article;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListArticlesTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function test_example()
+    {
+        //
+    }
+}
