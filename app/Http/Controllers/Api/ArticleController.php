f197a3c137e50183676e077dc41eee06420ee363
Diego Gonzalez <gonzalezdiego1990@gmail.com>
2021-08-06 15:34:19 +0000

    Obtiene todos los articulos

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 use App\Models\Article;
 use App\Http\Resources\ArticleResource;
+use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
     public function show(Article $article): ArticleResource
     {
         return ArticleResource::make($article);
+    }
+
+    public function index(): ArticleCollection
+    {
+        // Necesitamos devolver una colección de ArticleResource
+        // return ArticleResource::collection($articles);
+        return ArticleCollection::make(Article::all()); // Necesitamos crearlo para poder enviar la llave links y otra llave data con la colección de articulos y se llama al metodo make
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArticleController;
 
+Route::get('articles', [ArticleController::class, 'index'])->name('api.v1.articles.index');
+
 Route::get('articles/{article}', [ArticleController::class, 'show'])->name('api.v1.articles.show');
